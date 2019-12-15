3b5a1444e552d113ff472d3359640fbccbf71118
Marcos Ribeiro <mprribeiro1902@gmail.com>
2019-12-15 15:45:48 -0300

    Primeiro domínio, banco h2 e criação automática da base de dados

--- src/main/java/com/marcosribeiro/resources/CategoryResource.java
+++ src/main/java/com/marcosribeiro/resources/CategoryResource.java
@@ -1,15 +1,28 @@
 package com.marcosribeiro.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.marcosribeiro.domain.Category;
 
 @RestController
 @RequestMapping(value="/categories")
 public class CategoryResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String retrieve( ) {
-		return "Rest is OK!";
+	public List<Category> retrieve( ) {
+		Category cat1 = new Category(1, "Informática");
+		Category cat2 = new Category(2, "Escritório");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		return list;
 	}
+			
 }
