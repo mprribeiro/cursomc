f6c6b82dd2e569e31e457be5f1842923c70b97cd
Marcos <mprribeiro1902@gmail.com>
2020-02-01 19:33:51 -0300

    travis

--- src/main/java/com/marcosribeiro/resources/MainResource.java
+++ src/main/java/com/marcosribeiro/resources/MainResource.java
@@ -2,15 +2,16 @@ package com.marcosribeiro.resources;
 
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("")
 public class MainResource {
 
-	@RequestMapping(value="")
-	public void Main( ) {
-		System.out.println("Aplicação para aplicar conhecimentos de DevOps, Java 8, Spring e Angular.");
+	@RequestMapping(method=RequestMethod.GET)
+	public String Main( ) {
+		return "Aplicação para aplicar conhecimentos de DevOps, Java 8, Spring e Angular.";
 	}
 			
 }
