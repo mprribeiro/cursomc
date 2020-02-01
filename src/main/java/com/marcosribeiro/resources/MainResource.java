98e4174abcbd9ea475e0efb8461933951ab37247
Marcos <mprribeiro1902@gmail.com>
2020-02-01 19:18:10 -0300

    travis

---
+++ src/main/java/com/marcosribeiro/resources/MainResource.java
@@ -0,0 +1,15 @@
+package com.marcosribeiro.resources;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class MainResource {
+
+	public void Main( ) {
+		System.out.print("Aplicação para aplicar conhecimentos de DevOps, Java 8, Spring e Angular.");
+	}
+			
+}
