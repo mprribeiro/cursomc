e35025bcad4af3887d654b99dbca36c055979c7b
Marcos <mprribeiro1902@gmail.com>
2020-03-13 12:35:33 -0300

    upload

--- src/main/java/com/marcosribeiro/resources/UploadResource.java
+++ src/main/java/com/marcosribeiro/resources/UploadResource.java
@@ -8,10 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-
 
 @RestController
 @RequestMapping(value = "/upload")
@@ -19,16 +16,17 @@ public class UploadResource {
 
 	private static String UPLOADED_FOLDER = "/home/marcos/workspace/ionic_angular/cursomc-frontend/src/assets/clients_images";
 
-	@RequestMapping(value="/{id}", method = RequestMethod.POST)
+	@RequestMapping(value = "/{id}", method = RequestMethod.POST)
 	public String uploadFile(@RequestBody String value, @PathVariable String id) throws IOException {
-			byte[] imageByte = Base64.getDecoder().decode(value);
-			String path = UPLOADED_FOLDER + "/client_" + id + ".png";
-			FileOutputStream outputStream = new FileOutputStream(path);
-			outputStream.write(imageByte);
-			outputStream.flush();
-			outputStream.close();
-			System.out.println("Passou!");
-			return "Success";
+		System.out.println(value);
+		byte[] imageByte = Base64.getDecoder().decode(value);
+		String path = UPLOADED_FOLDER + "/client_" + id + ".png";
+		FileOutputStream outputStream = new FileOutputStream(path);
+		outputStream.write(imageByte);
+		outputStream.flush();
+		outputStream.close();
+		System.out.println("Passou!");
+		return "Success";
 	}
 
 }
