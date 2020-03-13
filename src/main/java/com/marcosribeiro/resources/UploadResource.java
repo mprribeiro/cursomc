939d4b618e014281c5642c6611bbfaed018a29d7
Marcos <mprribeiro1902@gmail.com>
2020-03-13 11:20:05 -0300

    upload

---
+++ src/main/java/com/marcosribeiro/resources/UploadResource.java
@@ -0,0 +1,33 @@
+package com.marcosribeiro.resources;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping(value = "/upload")
+public class UploadResource {
+
+	private static String UPLOADED_FOLDER = "/home/marcos/workspace/ionic_angular/cursomc-frontend/src/assets/clients_images";
+
+	@RequestMapping(value="/{id}", method = RequestMethod.POST)
+	public String uploadFile(@RequestParam("value") String value, @PathVariable String id) throws IOException {
+			byte[] imageByte = Base64.getDecoder().decode(value);
+			String path = UPLOADED_FOLDER + "/client_" + id + ".jpeg";
+			FileOutputStream outputStream = new FileOutputStream(path);
+			outputStream.write(imageByte);
+			outputStream.flush();
+			outputStream.close();
+			System.out.println("Passou!");
+			return "Success";
+	}
+
+}
